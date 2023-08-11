c1f0a2f16bfbea74f46059a33b2b72a03e38cb52
Arquimedes <108104701+Archads@users.noreply.github.com>
2023-08-11 12:01:56 -0700

    att tela_cadastro

--- app_med_base2023/lib/telas/cadastro_medico.dart
+++ app_med_base2023/lib/telas/cadastro_medico.dart
@@ -1,59 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:app_med_base2023/telas/login_Medico.dart';
 
-class CadMedico extends StatelessWidget {
+void main() {
+  runApp(CadMedico());
+}
+
+class CadMedico extends StatefulWidget {
+  @override
+  _CadMedicoState createState() => _CadMedicoState();
+}
+
+class _CadMedicoState extends State<CadMedico> {
+  bool showErrorText = false;
+
+  TextEditingController _fullNameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _professionalTypeController = TextEditingController();
+  TextEditingController _hospitalController = TextEditingController();
+  TextEditingController _crmController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Padding(
-          padding: EdgeInsets.only(top: 58, left: 30, right: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _Header(),
-              SizedBox(height: 40),
-              _TextField(label: 'Nome Completo'),
-              SizedBox(height: 13),
-              _TextField(label: 'Endereço de Email'),
-              SizedBox(height: 13),
-              _TextField(label: 'Tipo Profissional'),
-              SizedBox(height: 13),
-              _TextField(label: 'Hospital que trabalha'),
-              SizedBox(height: 13),
-              _TextField(label: 'Número CRM'),
-              SizedBox(height: 13),
-              _TextField(label: 'Senha', isPassword: true),
-              SizedBox(height: 13),
-              _TextField(label: 'Confirmar Senha', isPassword: true),
-              SizedBox(height: 10),
-              Row(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _CustomButton(
-                    imagePath: 'images/Bcadastrar.png',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SouMedico()),
-                      );
-                    },
-                  ),
-                  SizedBox(width: 10),
-                  _CustomButton(
-                    imagePath: 'images/Bvoltar4.png',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SouMedico()),
-                      );
-                    },
+                  _Header(),
+                  if (showErrorText) ...[
+                    SizedBox(height: 10),
+                    Text(
+                      'Por favor preencha todos os campos.',
+                      style: TextStyle(color: Colors.red),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                  SizedBox(height: 40),
+                  _centeredTextField(
+                      controller: _fullNameController, label: 'Nome Completo'),
+                  SizedBox(height: 13),
+                  _centeredTextField(
+                      controller: _emailController, label: 'Endereço de Email'),
+                  SizedBox(height: 13),
+                  _centeredTextField(
+                      controller: _professionalTypeController,
+                      label: 'Tipo Profissional'),
+                  SizedBox(height: 13),
+                  _centeredTextField(
+                      controller: _hospitalController,
+                      label: 'Hospital que trabalha'),
+                  SizedBox(height: 13),
+                  _centeredTextField(
+                      controller: _crmController, label: 'Número CRM'),
+                  SizedBox(height: 13),
+                  _centeredTextField(
+                      controller: _passwordController,
+                      label: 'Senha',
+                      isPassword: true),
+                  SizedBox(height: 13),
+                  _centeredTextField(
+                      controller: _confirmPasswordController,
+                      label: 'Confirmar Senha',
+                      isPassword: true),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _CustomButton(
+                        imagePath: 'images/Bcadastrar.png',
+                        onTap: () {
+                          bool anyFieldEmpty = false;
+                          if (_fullNameController.text.isEmpty ||
+                              _emailController.text.isEmpty ||
+                              _professionalTypeController.text.isEmpty ||
+                              _hospitalController.text.isEmpty ||
+                              _crmController.text.isEmpty ||
+                              _passwordController.text.isEmpty ||
+                              _confirmPasswordController.text.isEmpty) {
+                            anyFieldEmpty = true;
+                          }
+
+                          if (anyFieldEmpty) {
+                            setState(() {
+                              showErrorText = true;
+                            });
+                          } else {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SouMedico()),
+                            );
+                          }
+                        },
+                      ),
+                      SizedBox(width: 10),
+                      _CustomButton(
+                        imagePath: 'images/Bvoltar4.png',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SouMedico()),
+                          );
+                        },
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
@@ -64,28 +127,24 @@ class CadMedico extends StatelessWidget {
 class _Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
         Image.asset(
           'images/arquivomedico 1.png',
           width: 80,
           height: 100,
         ),
-        Flexible(
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.only(left: 0),
-              child: Text(
-                'Por favor preencha os campos com as informações corretas',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  color: Color(0xFF2c2b2b),
-                ),
-                textAlign: TextAlign.center,
-              ),
+        Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: Text(
+            'Por favor preencha os campos com as informações corretas',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              color: Color(0xFF2c2b2b),
             ),
+            textAlign: TextAlign.center,
           ),
         ),
       ],
@@ -93,11 +152,13 @@ class _Header extends StatelessWidget {
   }
 }
 
-class _TextField extends StatelessWidget {
+class _centeredTextField extends StatelessWidget {
+  final TextEditingController controller;
   final String label;
   final bool isPassword;
 
-  const _TextField({
+  const _centeredTextField({
+    required this.controller,
     required this.label,
     this.isPassword = false,
   });
@@ -105,7 +166,6 @@ class _TextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           label + ':',
@@ -114,6 +174,7 @@ class _TextField extends StatelessWidget {
             fontStyle: FontStyle.italic,
           ),
         ),
+        SizedBox(height: 5),
         Container(
           width: 334,
           height: 35,
@@ -122,6 +183,7 @@ class _TextField extends StatelessWidget {
             color: Color(0xFF0B99FF),
           ),
           child: TextField(
+            controller: controller,
             textAlign: TextAlign.center,
             obscureText: isPassword,
             decoration: InputDecoration(
@@ -157,8 +219,8 @@ class _CustomButton extends StatelessWidget {
       onTap: onTap,
       child: Image.asset(
         imagePath,
-        width: 30,
-        height: 50,
+        width: 50,
+        height: 70,
       ),
     );
   }
