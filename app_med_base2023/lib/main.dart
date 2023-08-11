ce421831a15e3ae795f85073d9ddea014fe57372
Arquimedes <108104701+Archads@users.noreply.github.com>
2023-08-11 11:04:15 -0700

    testes no firebase no firebase

--- app_med_base2023/lib/main.dart
+++ app_med_base2023/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:app_med_base2023/app_widget.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(AppWidget());
 }
