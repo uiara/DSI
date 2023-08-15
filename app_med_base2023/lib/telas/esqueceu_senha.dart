114ce78ab02b64fce330f50cab826f00e47e12e1
Wanderson Moura <wms10recife@gmail.com>
2023-08-15 16:09:06 -0300

    tela esqueceu senha

--- app_med_base2023/lib/telas/esqueceu_senha.dart
+++ app_med_base2023/lib/telas/esqueceu_senha.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
 
-class EsqueceuSenha extends StatelessWidget {
+class EsqueceuSenha extends StatefulWidget {
+  @override
+  _EsqueceuSenhaState createState() => _EsqueceuSenhaState();
+}
+
+class _EsqueceuSenhaState extends State<EsqueceuSenha> {
+  final TextEditingController _emailController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _enviarEmail() {
+    if (_formKey.currentState!.validate()) {
+      // Adicione aqui a lógica para enviar um e-mail de recuperação de senha
+      final email = _emailController.text;
+      // Exibir uma mensagem ou fazer outra ação
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Esqueci minha senha'),
       ),
-      body: Center(
-        child: Text('em construção'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Digite o seu e-mail para recuperar a senha:',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'Digite o seu e-mail',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, digite o seu e-mail';
+                  }
+                  if (!value.contains('@')) {
+                    return 'Por favor, insira um e-mail válido';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _enviarEmail,
+                child: Text('Enviar E-mail'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
