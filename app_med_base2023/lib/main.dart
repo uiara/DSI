68691a0bb4fd1b5b29946d4042d45ffa91ab23e3
Arquimedes <108104701+Archads@users.noreply.github.com>
2023-08-20 23:17:50 -0700

    integração firebase#1 conectado

--- app_med_base2023/lib/main.dart
+++ app_med_base2023/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:app_med_base2023/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:app_med_base2023/app_widget.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(AppWidget());
 }
