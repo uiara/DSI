2b27db93673c53bdecc92d2b3306cf50b05f38b5
Arquimedes <108104701+Archads@users.noreply.github.com>
2023-09-13 23:16:31 -0700

    deleção e dados complexos funcionando
    cor das letras mudas dependendo do valor vermelho=acima do normal azul=normal laranja=abaixo do normal

--- app_med_base2023/lib/telas/pacientes_Medico.dart
+++ app_med_base2023/lib/telas/pacientes_Medico.dart
@@ -9,9 +9,24 @@ class Patient {
   final String nome;
   final int idade;
   final double altura;
+  final double saturacaoOxigenio;
+  final int frequenciaCardiaca;
+  final double maiorConcentracaoGlicose;
+  final double menorConcentracaoGlicose;
   final String imageAsset;
+  final String notasPaciente;
 
-  Patient(this.nome, this.idade, this.altura, this.imageAsset);
+  Patient(
+    this.nome,
+    this.idade,
+    this.altura,
+    this.saturacaoOxigenio,
+    this.frequenciaCardiaca,
+    this.maiorConcentracaoGlicose,
+    this.menorConcentracaoGlicose,
+    this.imageAsset,
+    this.notasPaciente,
+  );
 }
 
 class PacMedico extends StatelessWidget {
@@ -64,12 +79,38 @@ class _PatientListScreenState extends State<PatientListScreen> {
           data['nome'],
           data['idade'],
           data['altura'],
-          'images/paciente 1.png', // Use the desired image
+          data['saturacaoOxigenio'] ?? 0.0,
+          data['frequenciaCardiaca'] ?? 0,
+          data['maiorConcentracaoGlicose'] ?? 0.0,
+          data['menorConcentracaoGlicose'] ?? 0.0,
+          'images/paciente 1.png',
+          data['notasPaciente'] ?? '',
         ));
       });
     }
 
     return patients;
+  }
+
+  Future<void> _deletarPaciente(Patient patient) async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      try {
+        await _firestore
+            .collection('users')
+            .doc(user.uid)
+            .collection('pacientes')
+            .doc() // Use o ID correto do documento que deseja excluir
+            .delete();
+
+        // Atualize a lista de pacientes após a exclusão
+        patients.remove(patient);
+        setState(() {});
+      } catch (error) {
+        // Trate erros de exclusão, se necessário
+        print('Erro ao deletar o paciente: $error');
+      }
+    }
   }
 
   @override
@@ -87,79 +128,159 @@ class _PatientListScreenState extends State<PatientListScreen> {
                     itemCount: patients.length,
                     itemBuilder: (context, index) {
                       final patient = patients[index];
-                      return InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => PerfPac(pacienteData: {
-                                'nome': patient.nome,
-                                'idade': patient.idade,
-                                'altura': patient.altura,
-                              }),
-                            ),
-                          );
-                        },
-                        child: Column(
+                      return ExpansionTile(
+                        title: Row(
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 8.0, horizontal: 16.0),
-                              child: Row(
-                                children: [
-                                  Container(
-                                    width: 97,
-                                    height: 97,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      image: DecorationImage(
-                                        image: AssetImage(patient.imageAsset),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(width: 25),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          'Nome: ${patient.nome}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
-                                              fontSize: 16,
-                                              fontStyle: FontStyle.italic),
-                                        ),
-                                        SizedBox(height: 8),
-                                        Text(
-                                          'Idade: ${patient.idade}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
-                                              fontSize: 16,
-                                              fontStyle: FontStyle.italic),
-                                        ),
-                                        SizedBox(height: 8),
-                                        Text(
-                                          'Altura: ${patient.altura.toStringAsFixed(2)}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
-                                              fontSize: 16,
-                                              fontStyle: FontStyle.italic),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  SizedBox(height: 25),
-                                ],
+                            Container(
+                              width: 97,
+                              height: 97,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage(patient.imageAsset),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                            SizedBox(height: 25),
+                            SizedBox(width: 25),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Nome: ${patient.nome}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Idade: ${patient.idade}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Altura: ${patient.altura.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ],
                         ),
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Saturação de oxigênio: ${patient.saturacaoOxigenio != null ? patient.saturacaoOxigenio.toStringAsFixed(2) : 'N/A'}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: (patient.saturacaoOxigenio != null)
+                                      ? (patient.saturacaoOxigenio > 105)
+                                          ? Colors.red
+                                          : (patient.saturacaoOxigenio >= 93 &&
+                                                  patient.saturacaoOxigenio <=
+                                                      105)
+                                              ? Colors.blue
+                                              : Colors.orange
+                                      : Colors
+                                          .black, // Cor padrão se o valor for nulo
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              Text(
+                                'Frequência cardíaca: ${patient.frequenciaCardiaca != null ? patient.frequenciaCardiaca.toStringAsFixed(2) : 'N/A'}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: (patient.frequenciaCardiaca != null)
+                                      ? (patient.frequenciaCardiaca > 100)
+                                          ? Colors.red
+                                          : (patient.frequenciaCardiaca >= 60 &&
+                                                  patient.frequenciaCardiaca <=
+                                                      100)
+                                              ? Colors.blue
+                                              : Colors.orange
+                                      : Colors
+                                          .black, // Cor padrão se o valor for nulo
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              Text(
+                                'Maior conc. de glicose: ${patient.maiorConcentracaoGlicose != null ? patient.maiorConcentracaoGlicose.toStringAsFixed(2) : 'N/A'}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: (patient.maiorConcentracaoGlicose !=
+                                          null)
+                                      ? (patient.maiorConcentracaoGlicose > 180)
+                                          ? Colors.red
+                                          : (patient.maiorConcentracaoGlicose >=
+                                                      140 &&
+                                                  patient.maiorConcentracaoGlicose <=
+                                                      180)
+                                              ? Colors.blue
+                                              : Colors.orange
+                                      : Colors
+                                          .black, // Cor padrão se o valor for nulo
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              Text(
+                                'Menor conc. de glicose: ${patient.menorConcentracaoGlicose != null ? patient.menorConcentracaoGlicose.toStringAsFixed(2) : 'N/A'}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: (patient.menorConcentracaoGlicose !=
+                                          null)
+                                      ? (patient.menorConcentracaoGlicose > 100)
+                                          ? Colors.red
+                                          : (patient.menorConcentracaoGlicose >=
+                                                      70 &&
+                                                  patient.menorConcentracaoGlicose <=
+                                                      100)
+                                              ? Colors.blue
+                                              : Colors.orange
+                                      : Colors
+                                          .black, // Cor padrão se o valor for nulo
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                              Text(
+                                'Notas: ${patient.notasPaciente}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black, // Cor para as notas
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: ElevatedButton(
+                              onPressed: () => _deletarPaciente(patient),
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.red,
+                              ),
+                              child: Text('Deletar'),
+                            ),
+                          ),
+                        ],
                       );
                     },
                   ),
@@ -217,3 +338,5 @@ class _PatientListScreenState extends State<PatientListScreen> {
     );
   }
 }
+
+void main() => runApp(PacMedico());
